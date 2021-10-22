2f6c5ba5583e765afe8ea80b4d6e9e71e39a4514
ИСП-31 <-31@LAB24-13>
2021-10-22 09:28:41 +0300

    Добавьте файлы проекта.

---
+++ Lib_13/Practice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_13
+{
+    public class Practice
+    {
+        public static void Power(out string numbers, out string results)
+        {
+            Random randomNumber = new Random();
+            double x = int.MinValue;
+            double copyX;
+            numbers = string.Empty;
+            results = string.Empty;
+            do
+            {
+                copyX = x;
+                x = randomNumber.Next(-5, 5);
+                numbers += Convert.ToString(x) + "; ";
+
+                double currentResult = default;
+
+                if (x > 0)
+                {
+                    currentResult = Math.Round(Math.Sqrt(x), 2);
+                }
+                if (x <= 0)
+                {
+                    currentResult = Math.Pow(x, 2);
+                }
+                results += Convert.ToString(currentResult) + "; ";
+
+            } while (x != copyX);
+
+            RemoveLastSeporator(ref numbers, ref results);
+        }
+
+        private static void RemoveLastSeporator(ref string numbers, ref string results)
+        {
+            numbers = numbers.Remove(numbers.LastIndexOf(";"));
+            results = results.Remove(results.LastIndexOf(";"));
+        }
+    }
+}
---
+++ prakt 1.1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_13;
+
+namespace prakt_1._1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            Practice.Power(out string numbers, out string results);
+            valueOut.Text = numbers;
+            rezultOut.Text = results;
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Савельев Дмитрий Александрович В13\nПрактическая работа №1\n.Генерировать случайные числа Х, распределенные в диапазоне от -5 до 5 и вычислять для чисел > 0 √X , а для чисел < 0 функцию x^2. Вычисления прекратить, когда подряд появится два одинаковых случайных числа. На экран необходимо выводить сгенерированное число и результат расчета функции на разных строках", "Информация о программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            valueOut.Clear();
+            rezultOut.Clear();
+        }
+    }
+}
